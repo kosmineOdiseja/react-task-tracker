7b60ca8f97ee59e0eb5c9e5196481543a69f8376
kosmineOdiseja <aliusbosas@gmail.com>
2021-08-27 20:30:59 +0300

    styles update

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 const Header = ({ title }) => {
 	return (
 		<header>
-			<h1 style={{color: 'red', backgroundColor: 'black'}}>{title}</h1>
+			<h1 style={headingStyle}>{title}</h1>
 		</header>			
 	)
 }
@@ -16,7 +16,7 @@ Header.propTypes = {
 }
 const headingStyle = {
 	color: 'red', 
-	backgroundColor: 'black'
-}
+	backgroundColor: 'black',
+};
 export default Header
 
